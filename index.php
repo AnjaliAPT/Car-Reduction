9b67b5ec8bd1af066b34843cb9aee52fddf2b785
Sneha Yadav <snehay5757@gmail.com>
2019-11-06 22:20:42 +0530

    login page added

--- index.php
+++ index.php
@@ -7,23 +7,25 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!--<link rel="stylesheet" href="css/main.css">-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 
 <body>
-    <nav class=" flex-container">
-        <div>
-            <a href="login.php"><button class="nav-button">LogIn</button></a>
-            <a href="signup.php"><button class="nav-button">SignUp</button></a>
-        </div>
-    </nav>
-    <main>
-
-    </main>
+<nav class="navbar navbar-light" style="background-color:#77CC6D">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#"></a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">LogIn</a></li>
+      <li><a href="#">SignUp</a></li>
+    </ul>
+  </div>
+</nav>
     <footer>
 
     </footer>
 </body>
-
 </html>
