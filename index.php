421813d92cf88f7732bd28848dc832ae38fe757c
AKASHCS50 <akcount121@gmail.com>
2019-12-12 17:01:16 +0530

    12/12/2019

--- index.php
+++ index.php
@@ -18,11 +18,11 @@
   <!-- Latest compiled JavaScript -->
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- Material Google icon theme -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
   <!-- CSS Stylesheet -->
-  <link rel="stylesheet" href="main.css">
+  <link rel="stylesheet" href="css/style.css">
 
 </head>
 
@@ -36,11 +36,11 @@
     </div>
     <div class="hd-header-overlay"></div>
     <nav class="row justify-content-between align-items-center">
-      <div class="logos col-md-2">
-        <a href="index.php"><img src="Resources/images/Group4.png" id="logo-img"></a>
+      <div class="logos col-md-3">
+        <a href="index.php"><img src="Resources/images/Group 4.png" id="logo-img"></a>
         <a href="index.php"><img src="Resources/images/img.png" alt="ferry" id="logo"></a>
       </div>
-      <div class="btns col-md-2">
+      <div class="btns col-md-4">
         <a href="#loginmodal" data-toggle="modal" id="login-btn" class="btn btn-outline-primary">Login</a>
         <a href="#signupmodal" data-toggle="modal" id="signup-btn" class="btn btn-outline-primary">SignUp</a>
       </div>
@@ -70,77 +70,124 @@
                 <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
                   <h3 class="register-heading">SignUp</h3>
                   <button type="button" class="close" data-dismiss="modal" aria-label="Close">&times;</button>
-                  <div class="row register-form">
-                    <div class="col-md-6">
-                      <div class="form-group">
-                        <input type="text" class="form-control" placeholder="First Name *" value="" />
-                      </div>
-                      <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Last Name *" value="" />
-                      </div>
-                      <div class="form-group">
-                        <input type="password" class="form-control" placeholder="Password *" value="" />
-                      </div>
-                      <div class="form-group">
-                        <input type="password" class="form-control" placeholder="Confirm Password *" value="" />
-                      </div>
-                      <div class="form-group">
-                        <div class="maxl">
-                          <label class="radio inline">
-                            <input type="radio" name="gender" value="male" checked>
-                            <span> Male </span>
-                          </label>
-                          <label class="radio inline">
-                            <input type="radio" name="gender" value="female">
-                            <span>Female </span>
-                          </label>
+
+
+                  <form action="php/signup_validation.php" class="needs-validation" method="post" novalidate>
+                    <div class="row register-form">
+                      <div class="col-md-6">
+                        <div class="form-group input-group">
+                          <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="material-icons">&#xe851;</i></span>
+                          </div>
+
+                          <input type="text" class="form-control" placeholder="First Name *" value="" name="name" required />
+                          <div class="valid-feedback"></div>
+                          <div class="invalid-feedback">Please fill out this field.</div>
+                        </div>
+                        <div class="form-group input-group">
+                          <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="material-icons">&#xe7ff;</i></span>
+                          </div>
+                          <input type="text" class="form-control" placeholder="Last Name *" value="" name="last_name" required />
+                          <div class="valid-feedback"></div>
+                          <div class="invalid-feedback">Please fill out this field.</div>
+                        </div>
+                        <div class="form-group input-group">
+                          <div class="input-group-prepend">
+                            <span class="input-group-text">@
+                          </div>
+                          <input type="email" class="form-control" placeholder="Email *" value="" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required />
+                          <div class="valid-feedback"></div>
+                          <div class="invalid-feedback">Invalid email.</div>
+                        </div>
+                        <div class="form-group input-group">
+                          <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="material-icons">&#xe0b0;</i></span>
+                            </span>
+                            <div class="valid-feedback"></div>
+                            <div class="invalid-feedback"> Invalid Mobile Number.</div>
+                          </div>
+
+                          <input type="tel" class="form-control" placeholder="Mobile Number *" value="" name="mobile" pattern="[789][0-9]{9}" required />
+                        </div>
+                        <div class="form-group">
+                          <div class="maxl">
+                            <span style="color:#8E959B">Gender : &nbsp;</span>
+                            <label class="radio inline">
+                              <input type="radio" name="gender" value="male" checked>
+                              <span> Male </span>
+                            </label>
+                            <label class="radio inline">
+                              <input type="radio" name="gender" value="female">
+                              <span>Female </span>
+                            </label>
+                          </div>
                         </div>
                       </div>
+                      <div class="col-md-6">
+                        <div class="custom-control custom-checkbox form-group">
+                          <input type="checkbox" class="custom-control-input" id="customCheck" name="car_binary">
+                          <label class="custom-control-label" for="customCheck">Do you have a car :&nbsp; </label>
+                        </div>
+
+                        <div class="form-group">
+                          <input type="text" class="form-control" id="np" placeholder="Registration Number *" value="" name="registration_no" disabled required />
+                          <div class="valid-feedback"></div>
+                          <div class="invalid-feedback"> Please fill out this field.</div>
+                        </div>
+                        <div class="form-group">
+                          <input type="text" class="form-control" id="nr" placeholder="Car Model *" value="" name="car_type" disabled required />
+                          <div class="valid-feedback"></div>
+                          <div class="invalid-feedback"> Please fill out this field.</div>
+                        </div>
+                        <div class="form-group">
+                          <input type="text" class="form-control" id="ns" placeholder="Car Number *" value="" name="car_no" disabled required />
+                        </div>
+                        <div class="valid-feedback"></div>
+                        <div class="invalid-feedback"> Please fill out this field.</div>
+                      </div>
+                      <div class="col-md-6">
+                        <input type="submit" class="btnRegister" value="Register" />
+                      </div>
+
                     </div>
-                    <div class="col-md-6">
-                      <div class="form-group">
-                        <input type="email" class="form-control" placeholder="Your Email *" value="" />
-                      </div>
-                      <div class="form-group">
-                        <input type="text" minlength="10" maxlength="10" name="txtEmpPhone" class="form-control" placeholder="Your Phone *" value="" />
-                      </div>
-                      <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Enter Your Answer *" value="" />
-                      </div>
-                      <input type="submit" class="btnRegister" value="Register" />
-                    </div>
-                  </div>
+                  </form>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-  <!-- End of Form -->
+      <!-- End of Form -->
 
 
 
-  <section class="section section-a">
-    <div class="container">
-      <h2>Section A</h2>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, impedit amet minima iste autem cumque et
-        maiores blanditiis doloribus aut dolorum quaerat non est voluptatum, tempore ut dolorem voluptas quod quae
-        accusantium, ex inventore ducimus. Beatae mollitia exercitationem, quam similique, consectetur ratione
-        reprehenderit delectus neque eligendi facere soluta dolor ducimus!</p>
-    </div>
-  </section>
+      <section class="section section-a">
+        <div class="container">
+          <h2>Section A</h2>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, impedit amet minima iste autem cumque et
+            maiores blanditiis doloribus aut dolorum quaerat non est voluptatum, tempore ut dolorem voluptas quod quae
+            accusantium, ex inventore ducimus. Beatae mollitia exercitationem, quam similique, consectetur ratione
+            reprehenderit delectus neque eligendi facere soluta dolor ducimus!</p>
+        </div>
+      </section>
 
-  <section class="section section-b">
-    <div class="container">
-      <h2>Section B</h2>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, impedit amet minima iste autem cumque et
-        maiores blanditiis doloribus aut dolorum quaerat non est voluptatum, tempore ut dolorem voluptas quod quae
-        accusantium, ex inventore ducimus. Beatae mollitia exercitationem, quam similique, consectetur ratione
-        reprehenderit delectus neque eligendi facere soluta dolor ducimus!</p>
-    </div>
-  </section>
+      <section class="section section-b">
+        <div class="container">
+          <h2>Section B</h2>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, impedit amet minima iste autem cumque et
+            maiores blanditiis doloribus aut dolorum quaerat non est voluptatum, tempore ut dolorem voluptas quod quae
+            accusantium, ex inventore ducimus. Beatae mollitia exercitationem, quam similique, consectetur ratione
+            reprehenderit delectus neque eligendi facere soluta dolor ducimus!</p>
+        </div>
+      </section>
+
+
+      <!-- Javascript -->
+      <script src="JS/car_binary.js">
+      </script>
+      <script src="JS/form_validation.js"></script>
+      <!-- End of Javascript -->
 </body>
 
 </html>
---
+++ php/signup_validation.php
@@ -0,0 +1,71 @@
+<?php
+ob_start();
+// print_r($_POST);
+
+foreach ($_POST as $key => $value)
+    echo $key . '=' . $value . '<br />';
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    header('Cache-Control: no cache'); //no cache
+    session_cache_limiter('private_no_expire'); // works
+    //session_cache_limiter('public'); // works too
+    session_start();
+    include('connect.php');
+    $f_name = mysqli_real_escape_string($db, $_POST["name"]);
+    $l_name = mysqli_real_escape_string($db, $_POST["last_name"]);
+    $e = mysqli_real_escape_string($db, $_POST["email"]);
+    $mob = mysqli_real_escape_string($db, $_POST["mobile"]);
+    $gend = mysqli_real_escape_string($db, $_POST["gender"]);
+
+
+
+    $hsh = md5(rand(0, 1000));
+    $pswd = rand(10000, 99999);
+    $hash_pswd = md5($pswd);
+    $user = $f_name . $pswd;
+
+
+    //     $to      = $email; 
+    //     $subject = 'Signup | Verification'; 
+    //     $message = '
+
+    //     Thanks for signing up!
+    //     Your account has been created, you can login with the following credentials after you have activated your account by pressing the url below.
+
+    //     ------------------------
+    //     Username: ' . $username . '
+    //     Password: ' . $password . '
+    //     ------------------------
+
+    //     Please click this link to activate your account:
+    //     http://www.yourwebsite.com/verify.php?email=' . $email . '&hash=' . $hash . '
+
+    // '; 
+
+    //     $headers = 'From:noreply@yourwebsite.com' . "\r\n"; 
+    //     mail($to, $subject, $message, $headers); 
+
+    if (isset($_POST["car_binary"]))
+        $car_bin = 1;
+    else
+        $car_bin = 0;
+    $query = "SELECT * FROM user;";
+    $result = mysqli_query($db, $query);
+    $count = mysqli_num_rows($result);
+    $count = $count + 1;
+    $query = "INSERT INTO `user`(`id`, `first_name`, `last_name`,`email`, `mobile`, `gender`, `password`, `car_binary`, `email_binary`, `username`, `hash`) VALUES ($count,'$f_name','$l_name','$e', '$mob', '$gend','$pswd',$car_bin,0,'$user','$hsh');";
+    mysqli_query($db, $query);
+    // ob_end_clean();
+    // die();
+}
+session_destroy();
+
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
