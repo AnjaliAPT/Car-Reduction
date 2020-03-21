30e914262d61f7f283a57cc1b8e1a8f9b483342b
AKASH SHARMA <akcount121@gmail.com>
2020-03-21 18:10:07 +0530

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,6 +1,8 @@
  <?php
 
   //  $db = mysqli_connect('localhost', 'id11401863_dbms', 'my000webhost', 'id11401863_mysqlonlineforum') or die('Could not connect to database');
-  $db = mysqli_connect('localhost:3307', 'root', 'root', 'car_reduction') or die('Could not connect to database');
+  // $db = mysqli_connect('localhost:3307', 'root', 'root', 'car_reduction') or die('Could not connect to database');
+
+$db = mysqli_connect('remotemysql.com', 'JAXvCeJL1R', 'mTtbAMHi6k', 'JAXvCeJL1R') or die('Could not connect to database');
 
   ?>
